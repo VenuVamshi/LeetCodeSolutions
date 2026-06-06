90916d3c6ab84d51c24729617f5e468691c21090
Venu Vamshi R <venuvamshi9999@gmail.com>
2026-06-06 14:33:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (86.01%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+
         int n = nums.size();
-        int globalProduct = INT_MIN;
-        for(int i = 0; i < n ; i++){
-            int localProduct = 1;
-            for(int j = i; j < n; j++){
-                localProduct *= nums[j];
-                globalProduct = max(globalProduct,localProduct);
-            }
+        int globalMax = nums[0];
+        int maxProd = nums[0];
+        int minProd = nums[0];
+
+        for(int i = 1; i < n ; i++){
+            int x = nums[i];
+
+            int newMaxProd = max( { x , x * minProd , x * maxProd } );
+            int newMinProd = min( { x , x * minProd , x * maxProd } );
+
+            maxProd = newMaxProd;
+            minProd = newMinProd;
+
+            globalMax = max ( globalMax , maxProd );
+
         }
-        return globalProduct;
+        return globalMax;
     }
 };
