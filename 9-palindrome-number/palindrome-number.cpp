de372b832a2e0e7cb0a5da43d9520a868e691cea
Venu Vamshi R <venuvamshi9999@gmail.com>
2026-06-15 14:39:26 +0530

    Time: 6 ms (17.27%) | Memory: 8.7 MB (23.09%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -8,6 +8,17 @@ public:
         return true;
     }
     bool isPalindrome(int x) {
-        return isPalin(to_string(x));
+        //return isPalin(to_string(x));
+        if( x < 0) return false;
+        long temp = x;
+        long revnum = 0;
+        while(temp > 0){
+            long lastdigit = temp % 10;
+            revnum = 10*revnum + lastdigit;
+            temp = temp / 10;
+        }
+        return revnum == x;
+
+        
     }
 };
