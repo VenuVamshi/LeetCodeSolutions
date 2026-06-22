b7e90676f75406eca5bfdf1538b4c28f78716325
Venu Vamshi R <venuvamshi9999@gmail.com>
2026-06-22 10:35:28 +0530

    Time: 2 ms (30.90%) | Memory: 28.3 MB (27.45%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-         //S1 init required DSs
-    unordered_map<int, int> freq;
-    //S2 fill the freq table
-    for (auto num : nums) freq[num]++;
-    //S3 search fs greater than n by 2
-    for (auto p : freq) {
-        if (p.second > nums.size() / 2) return p.first;
+     // S1 sort the vector
+    sort(nums.begin(), nums.end());
+    //S2-return mid element 
+    return nums[nums.size() / 2];
     }
-    return -1;
-    }
-    
 };
