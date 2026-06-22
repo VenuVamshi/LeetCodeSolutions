cf7be6afb8983fe9d417340badbaa3f8b3cfb2eb
Venu Vamshi R <venuvamshi9999@gmail.com>
2026-06-22 13:55:57 +0530

    Time: 4 ms (45.56%) | Memory: 23.7 MB (62.39%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,34 +2,80 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
 
-        // Bring equal elements together
-        sort(nums.begin(), nums.end());
+        // Two possible majority candidates
+        int candidateElement1 = 0;
+        int candidateElement2 = 1;
+
+        // Vote margins of the candidates
+        int voteBalance1 = 0;
+        int voteBalance2 = 0;
+
+        // Phase 1: Find possible majority candidates
+        for (int num : nums) {
+
+            // Support candidate 1
+            if (num == candidateElement1) {
+
+                voteBalance1++;
+            }
+
+            // Support candidate 2
+            else if (num == candidateElement2) {
+
+                voteBalance2++;
+            }
+
+            // Elect a new candidate 1
+            else if (voteBalance1 == 0) {
+
+                candidateElement1 = num;
+                voteBalance1 = 1;
+            }
+
+            // Elect a new candidate 2
+            else if (voteBalance2 == 0) {
+
+                candidateElement2 = num;
+                voteBalance2 = 1;
+            }
+
+            // Cancel one vote from both candidates
+            else {
+
+                voteBalance1--;
+                voteBalance2--;
+            }
+        }
+
+        // Phase 2: Verify actual frequencies
+        int frequency1 = 0;
+        int frequency2 = 0;
+
+        for (int num : nums) {
+
+            if (num == candidateElement1) {
+
+                frequency1++;
+            }
+
+            else if (num == candidateElement2) {
+
+                frequency2++;
+            }
+        }
 
         vector<int> answer;
 
-        // Frequency of current group
-        int frequency = 1;
+        // Add candidate 1 if frequency exceeds n/3
+        if (frequency1 > nums.size() / 3) {
 
-        for (int i = 1; i <= nums.size(); i++) {
+            answer.push_back(candidateElement1);
+        }
 
-            // Extend current group
-            if (i < nums.size() && nums[i] == nums[i - 1]) {
+        // Add candidate 2 if frequency exceeds n/3
+        if (frequency2 > nums.size() / 3) {
 
-                frequency++;
-            }
-
-            // Current group ends
-            else {
-
-                // Add element if frequency exceeds n/3
-                if (frequency > nums.size() / 3) {
-
-                    answer.push_back(nums[i - 1]);
-                }
-
-                // Start counting next group
-                frequency = 1;
-            }
+            answer.push_back(candidateElement2);
         }
 
         return answer;
