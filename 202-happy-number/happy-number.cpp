95a56516bbca84ae9b3529a775a9dbfd75e0ef6b
Venu Vamshi R <venuvamshi9999@gmail.com>
2026-06-26 14:10:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (22.45%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     bool isHappy(int n){
-        unordered_set<int> s;
         
+        //s1-declare and init required DSs
+        unordered_set<int> s;
         int temp = n;
+        //s2-loop infinitely until either conditions are met
         while(1){
-        int digitSquareSum = 0;
-        while(temp > 0){
-            int lastDigit = temp % 10;
-            digitSquareSum += lastDigit*lastDigit;
-            temp /= 10;
-        }
-        if(digitSquareSum == 1) return true;
-        else if(s.count(digitSquareSum)) return false;
-        else s.insert(digitSquareSum);
-        temp = digitSquareSum;
+            int digitSquareSum = 0;
+            //Compute digitSquareSum
+            while(temp > 0){
+                int lastDigit = temp % 10;
+                digitSquareSum += lastDigit*lastDigit;
+                temp /= 10;
+            }
+            //check 3 mutually exclusive conditions 
+            //mec1 found happy number return 1
+            if(digitSquareSum == 1) return true;
+            //mec2 digitSqureSum is already in set so infinite loop so return false
+            else if(s.count(digitSquareSum)) return false;
+            //mec 3 insert digitSquareSum to set
+            else s.insert(digitSquareSum);
+            // place digitSquareSum in place of temp;
+            temp = digitSquareSum;
         }
 
     }
