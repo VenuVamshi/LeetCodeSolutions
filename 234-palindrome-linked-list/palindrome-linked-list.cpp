6c04502f1a8bb10940ca53dc14d4d94444fc1c99
Venu Vamshi R <venuvamshi9999@gmail.com>
2026-08-17 12:29:59 +0530

    Time: 4 ms (48.66%) | Memory: 132.1 MB (8.49%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // ListNode* reverseLL(ListNode* head){
+    //     ListNode* prev = NULL;
+    //     ListNode* curr = head;
+    //     ListNode* next = NULL;
+
+    //     while(curr){
+
+    //         next = curr->next;
+
+    //         curr->next = prev;
+
+    //         prev = curr;
+    //         curr = next;
+
+    //     }
+    //     return prev;
+    // }
+    bool isPalindrome(ListNode* head) {
+        // ListNode* h1 = head;
+        // ListNode* h2 = reverseLL(h1);
+        // ListNode* t1 = h1;
+        // ListNode* t2 = h2;
+        // while(t1 && t2){
+        //     if(t1->val != t2->val) return false;
+        //     t1 = t1->next ;
+        //     t2 = t2->next;
+        // }
+        // return true;
+
+        vector<int> v;
+        ListNode* cur = head;
+        while(cur){
+            v.push_back(cur->val);
+            cur = cur->next;
+        }
+
+        int low = 0;
+        int high = v.size()-1;
+
+        while(low < high){
+            if(v[low] != v[high]){
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
+};
